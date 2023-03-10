709b014c6c0a2fe1211346eef22d74066aa31860
Saurabh Wankhade <wankhadesaurabh30@gmail.com>
2023-03-10 23:21:32 +0530

    Final Addition Program Done

---
+++ Add.java
@@ -0,0 +1,12 @@
+class Add
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        System.out.println(" The sum is "+(a+b));
+	System.out.println("Saurabh Wankhade");
+    }
+
+}
+
